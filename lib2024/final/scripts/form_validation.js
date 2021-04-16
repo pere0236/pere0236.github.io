9bf37a3192da60b359f1bc470fdeb0cdfb922421
pere0236 <77305273+pere0236@users.noreply.github.com>
2021-04-16 15:13:33 -0400

    final

---
+++ lib2024/final/scripts/form_validation.js
@@ -0,0 +1,29 @@
+window.onload = init;
+function init(){
+
+$("form").submit(function(event){
+
+});
+
+$("form").submit(function(e) {
+	e.preventDefault ();
+$("#results").removeClass('reveal');
+	var form =this;
+	console.log(this);
+	$(".overlay-container").fadeIn(1000, function(){
+		showformValues(form);
+		$('.overlay-container')delay(500).fadeOut(500)
+	$("#results").addClass('reveal');
+})
+});
+function showforValues(form){
+	var formVaules =$(form).serializeArray ();
+	console.log(formValues);
+$.each(formVaule, function(index,field))
+console.log($('#' + field.name + "_result");
+var input_value =$.trim(field.value);
+$("#results").find("#"=field.name+"_result").text(input_value);
+if(field.name=="email"){
+	$("#results").find("#"+field.name+"_result").
+		attr("href", "malito:"+field.value);
+}
